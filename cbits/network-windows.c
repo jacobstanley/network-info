22f66fa34a0b88ba8777a3a35250e7e487065d31
Jacob Stanley <jacob@stanley.io>
2010-07-31 23:12:15 +0800

    Small cleanups to network-windows.c

--- cbits/network-windows.c
+++ cbits/network-windows.c
@@ -15,19 +15,19 @@ int get_adapters_addresses(IP_ADAPTER_ADDRESSES *adapters, ULONG *size) {
 }
 
 int c_get_network_interfaces(struct network_interface *ns, int max_ns) {
-    ULONG buffer_size;
     IP_ADAPTER_ADDRESSES *adapters, *adapter;
     IP_ADAPTER_UNICAST_ADDRESS *unicast;
+    ULONG buffer_size;
     DWORD error;
     int i = 0;
 
-    /* make an initial call to GetAdaptersInfo to get
-       the necessary size into the buffer_size variable */
+    /* make an initial call to get the necessary
+     * size into the buffer_size variable */
     error = get_adapters_addresses(NULL, &buffer_size);
 
     if (error != ERROR_BUFFER_OVERFLOW) {
         /* if we didn't get ERROR_BUFFER_OVERFLOW
-           then buffer_size was not set */
+         * then buffer_size was not set */
         return 0;
     }
 
