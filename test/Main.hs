133acba0151b549288ea80911097cefb5b94a402
Jacob Stanley <jacob@stanley.io>
2010-08-01 15:17:37 +0800

    Test program gives a nicer output

--- test/Main.hs
+++ test/Main.hs
@@ -3,5 +3,11 @@ module Main (main) where
 import Network.Info
 
 main = do
-    ifcs <- getNetworkInterfaces
-    mapM print ifcs
+    ifs <- getNetworkInterfaces
+    mapM (putStrLn . showInterface) ifs
+
+showInterface :: NetworkInterface -> String
+showInterface n = netName n ++ "\n"
+               ++ "IP Address:   " ++ show (netIP n) ++ "\n"
+               ++ "IPv6 Address: " ++ show (netIP6 n) ++ "\n"
+               ++ "MAC Address:  " ++ show (netMAC n) ++ "\n"
