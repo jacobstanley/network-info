88f54405ceba9bb690fbc91d966e25895829ad83
Jacob Stanley <jacob@stanley.io>
2010-08-10 03:39:25 +0800

    Cleanup cbits code style

--- cbits/common.h
+++ cbits/common.h
@@ -1,21 +1,26 @@
-inline int min(int a, int b) {
+inline int min(int a, int b)
+{
     return a < b ? a : b;
 }
 
-inline int ipv4copy(ipv4 *dst, struct sockaddr *addr) {
+inline void ipv4copy(ipv4 *dst, struct sockaddr *addr)
+{
     *dst = ((struct sockaddr_in *)addr)->sin_addr.s_addr;
 }
 
-inline int ipv6copy(ipv6 *dst, struct sockaddr *addr) {
+inline void ipv6copy(ipv6 *dst, struct sockaddr *addr)
+{
     memcpy(dst, ((struct sockaddr_in6 *)addr)->sin6_addr.s6_addr, sizeof(ipv6));
 }
 
-inline int wszcopy(wchar_t *dst, const wchar_t *src, size_t dst_size) {
+inline void wszcopy(wchar_t *dst, const wchar_t *src, size_t dst_size)
+{
     wcsncpy(dst, src, dst_size - 1);
     dst[dst_size - 1] = '\0';
 }
 
-inline int mbswszcopy(wchar_t *dst, const char *src, size_t dst_size) {
+inline void mbswszcopy(wchar_t *dst, const char *src, size_t dst_size)
+{
     mbstowcs(dst, src, dst_size - 1);
     dst[dst_size - 1] = '\0';
 }
--- cbits/network-windows.c
+++ cbits/network-windows.c
@@ -10,16 +10,19 @@
 #include "common.h"
 
 
-int get_adapters_addresses(IP_ADAPTER_ADDRESSES *adapters, ULONG *size) {
+int get_adapters_addresses(IP_ADAPTER_ADDRESSES *adapters, ULONG *size)
+{
     return GetAdaptersAddresses(AF_UNSPEC, 0, 0, adapters, size);
 }
 
-int c_get_network_interfaces(struct network_interface *ns, int max_ns) {
+int c_get_network_interfaces(struct network_interface *ns, int max_ns)
+{
+    struct sockaddr *addr;
     IP_ADAPTER_ADDRESSES *adapters, *adapter;
     IP_ADAPTER_UNICAST_ADDRESS *unicast;
     ULONG buffer_size;
     DWORD error;
-    int i = 0;
+    int family, i;
 
     /* make an initial call to get the necessary
      * size into the buffer_size variable */
@@ -33,6 +36,7 @@ int c_get_network_interfaces(struct network_interface *ns, int max_ns) {
 
     adapters = malloc(buffer_size);
     error = get_adapters_addresses(adapters, &buffer_size);
+    i = 0;
 
     if (error == NO_ERROR) {
         adapter = adapters;
@@ -42,11 +46,12 @@ int c_get_network_interfaces(struct network_interface *ns, int max_ns) {
             memcpy(ns[i].mac_address, adapter->PhysicalAddress, MAC_SIZE);
 
             for (unicast = adapter->FirstUnicastAddress; unicast; unicast = unicast->Next) {
-                struct sockaddr *addr = unicast->Address.lpSockaddr;
+                addr = unicast->Address.lpSockaddr;
+                family = addr->sa_family;
 
-                if (addr->sa_family == AF_INET) {
+                if (family == AF_INET) {
                     ipv4copy(&ns[i].ip_address, addr);
-                } else if (addr->sa_family == AF_INET6) {
+                } else if (family == AF_INET6) {
                     ipv6copy(&ns[i].ip6_address, addr);
                 }
             }
