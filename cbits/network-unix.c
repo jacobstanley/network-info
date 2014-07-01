jacobstanley/network-info

commit 7faccfa566ecacdfcb2a467b1037cd8b1099360e

Generalize to support Hurd

cbits/network-unix.c
@@ -33,7 +33,7 @@
 #include "network.h"
 #include "common.h"
 
-#ifdef __sun
+#if defined(__sun) || !defined(AF_PACKET)
 int maccopy_arp(unsigned char *dst, struct sockaddr *addr)
 {
     // SOURCE DERIVED FROM: http://www.pauliesworld.org/project/getmac.c
@@ -130,14 +130,14 @@ int c_get_network_interfaces(struct network_interface *ns, int max_ns)
         family = addr->sa_family;
         if (family == AF_INET) {
             ipv4copy(&n->ip_address, addr);
-#ifdef __sun
+#if defined(__sun) || !defined(AF_PACKET)
             if ((ifa->ifa_flags & IFF_LOOPBACK) == 0) {
                 maccopy_arp(n->mac_address, addr);
             }
 #endif
         } else if (family == AF_INET6) {
             ipv6copy(&n->ip6_address, addr);
-#ifdef __sun
+#if defined(__sun) || !defined(AF_PACKET)
             if ((ifa->ifa_flags & IFF_LOOPBACK) == 0) {
                 maccopy_arp(n->mac_address, addr);
             }

commit fdde6ad5b7089b18e8b8463c5216ffeeab9417a7

Fix build on Hurd
Build broken by 7faccfa. At the moment SIOCGARP is not defined on Hurd.

cbits/network-unix.c
@@ -18,10 +18,12 @@
 #         define AF_PACKET AF_LINK
 #      endif
 #   endif
-#   ifdef __sun
+#   if defined(__sun) || defined(__GNU__)
 #      include <net/if_arp.h>
-#      include <unistd.h>
 #      include <stropts.h>
+#   endif
+#   ifdef __sun
+#      include <unistd.h>
 #      include <sys/sockio.h>
 #   endif
 #endif
@@ -33,7 +35,7 @@
 #include "network.h"
 #include "common.h"
 
-#if defined(__sun) || !defined(AF_PACKET)
+#if defined(__sun) || (!defined(AF_PACKET) && defined(SIOCGARP))
 int maccopy_arp(unsigned char *dst, struct sockaddr *addr)
 {
     // SOURCE DERIVED FROM: http://www.pauliesworld.org/project/getmac.c
@@ -130,14 +132,14 @@ int c_get_network_interfaces(struct network_interface *ns, int max_ns)
         family = addr->sa_family;
         if (family == AF_INET) {
             ipv4copy(&n->ip_address, addr);
-#if defined(__sun) || !defined(AF_PACKET)
+#if defined(__sun) || (!defined(AF_PACKET) && defined(SIOCGARP))
             if ((ifa->ifa_flags & IFF_LOOPBACK) == 0) {
                 maccopy_arp(n->mac_address, addr);
             }
 #endif
         } else if (family == AF_INET6) {
             ipv6copy(&n->ip6_address, addr);
-#if defined(__sun) || !defined(AF_PACKET)
+#if defined(__sun) || (!defined(AF_PACKET) && defined(SIOCGARP))
             if ((ifa->ifa_flags & IFF_LOOPBACK) == 0) {
                 maccopy_arp(n->mac_address, addr);
             }